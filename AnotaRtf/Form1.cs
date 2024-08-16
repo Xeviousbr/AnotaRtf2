55ea28b7388ec942a955393e7f6f81a929ee1497
Xeviousbr <xeviousbr@gmail.com>
2024-08-16 01:22:29 -0300

    Atualização dos fontes do projeto

---
+++ AnotaRtf/Form1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AnotaRtf
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"AnoteitorRTF\MyApp"))
+                {
+                    if (key != null)
+                    {
+                        int x = (int)key.GetValue("WindowPositionX", this.Left);
+                        int y = (int)key.GetValue("WindowPositionY", this.Top);
+                        int width = (int)key.GetValue("WindowWidth", this.Width);
+                        int height = (int)key.GetValue("WindowHeight", this.Height);
+
+                        this.StartPosition = FormStartPosition.Manual;
+                        this.Left = x;
+                        this.Top = y;
+                        this.Width = width;
+                        this.Height = height;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // 
+            }
+            rtfTexto.caminhoDoArquivo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "anotacoes.rtf");
+            rtfTexto.Criptografia = false;
+            rtfTexto.Carrega();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"AnoteitorRTF\MyApp"))
+                {
+                    if (key != null)
+                    {
+                        key.SetValue("WindowPositionX", this.Left);
+                        key.SetValue("WindowPositionY", this.Top);
+                        key.SetValue("WindowWidth", this.Width);
+                        key.SetValue("WindowHeight", this.Height);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // 
+            }
+        }
+    }
+}
